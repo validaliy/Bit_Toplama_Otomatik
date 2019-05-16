c9544f608dd210c226da8de743c6f4f43b8a01f2
ibrahim Beylem <validaliy@hotmail.com>
2019-05-16 19:04:31 +0300

    pycharm düzeltti

--- python/bit_toplama_python.py
+++ python/bit_toplama_python.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 # 505 X 65 /  860 X 680 pixeller
 # 776x96 sola git / 836x96 sağa git / 808x67 yukarı / 808x132 aşağı
@@ -11,28 +10,31 @@ import win32api, win32con
 ekran_bekleme = 10
 yontem = "sıralı"
 yon = "sağ"
-
 """ dizide 0 x konumu, 1 y konumu, 2 3"""
 # Butonlar
-carpi =[838, 138]
+carpi = [838, 138]
 sari_x = [554, 105, 241, 176, 12]
-tamam = [587, 404, 250, 0, 0] # günlük bit toplama bitti veya konum doğrulanmadı hatası
+tamam = [587, 404, 250, 0, 0]  # günlük bit toplama bitti veya konum doğrulanmadı hatası
 
-#konum
+# konum
 konum_sag = [836, 96]
 konum_sol = [776, 96]
 konum_yukari = [808, 67]
 konum_asagi = [808, 132]
-konum_saniye = 1 # konum değiştirme tuşuna basılı tutma süresi
+konum_saniye = 1  # konum değiştirme tuşuna basılı tutma süresi
 
-#istatistik
+# istatistik
 toplandi = 0
-def tikla(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-    #time.sleep(0.00001)
-def git(yon = "sağ"):
+
+
+def tikla(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    # time.sleep(0.00001)
+
+
+def git(yon="sağ"):
     if yon == "sağ":
         pyautogui.mouseDown(konum_sag[0], konum_sag[1])
         time.sleep(konum_saniye)
@@ -53,10 +55,11 @@ def git(yon = "sağ"):
     else:
         print(saat(), " yukarı, aşağı, sol ve sağ yönler vardır sadece")
 
-def bittopla(yontem = "random"):
+
+def bittopla(yontem="random"):
     global toplandi
     toplandi += 1
-    print(saat(), " ", str(toplandi),". kez bit toplanmaya başlandı. yöntem: ", yontem)
+    print(saat(), " ", str(toplandi), ". kez bit toplanmaya başlandı. yöntem: ", yontem)
     time.sleep(3)
     tikla(690, 420)
     if yontem == "random":
@@ -103,8 +106,10 @@ def bittopla(yontem = "random"):
     else:
         pyautogui.alert(text="sadece random, ara, ara_resimli ve sıralı yöntemleri var.", title="Uyarı", button="Tamam")
 
+
 def saat():
     return (str(time.strftime('%X')))
+
 
 def bittimi():
     x_isareti = pyautogui.pixelMatchesColor(sari_x[0], sari_x[1], (sari_x[2], sari_x[3], sari_x[4]))
@@ -114,28 +119,26 @@ def bittimi():
         return True
 
 
-
 def check():
     while True:
         bekleniyor = pyautogui.pixelMatchesColor(700, 389, (36, 8, 16))
-        if bekleniyor: # ana ekranda bir şey dönüyor. onun bitmesini bekliyor
+        if bekleniyor:  # ana ekranda bir şey dönüyor. onun bitmesini bekliyor
             print(saat(), " bekleniyo")
             time.sleep(10)
             continue
 
-        paylas = pyautogui.pixelMatchesColor(580, 580, (250, 0, 0)) #arkadaşınla paylaş (tebrikler ekranı)
+        paylas = pyautogui.pixelMatchesColor(580, 580, (250, 0, 0))  # arkadaşınla paylaş (tebrikler ekranı)
         if paylas:
             tikla(carpi[0], carpi[1])
             print(saat(), " tebrikler ekranında çarpıya basıldı")
             time.sleep(3)
             continue
         devam_et = pyautogui.pixelMatchesColor(580, 580, (156, 42, 160))
-        if devam_et: #seviye atladınız ekranı
+        if devam_et:  # seviye atladınız ekranı
             print(saat(), " devam et butonuna tıklanacak")
             tikla(580, 580)
             time.sleep(3)
             continue
-
 
         oyunbasla_btn = pyautogui.pixelMatchesColor(550, 650, (250, 0, 0))
         if oyunbasla_btn:
@@ -153,7 +156,8 @@ def check():
             time.sleep(4)
             continue
 
-        bittoplamaEkrani = pyautogui.pixelMatchesColor(sari_x[0], sari_x[1], (sari_x[2], sari_x[3], sari_x[4])) # sari_x var
+        bittoplamaEkrani = pyautogui.pixelMatchesColor(sari_x[0], sari_x[1],
+                                                       (sari_x[2], sari_x[3], sari_x[4]))  # sari_x var
         if bittoplamaEkrani:
             bittopla(yontem)
             continue
@@ -164,4 +168,6 @@ def check():
 
         print(saat(), " Tanıdık ekrana rastlanmadı. ", str(ekran_bekleme), " saniye beklenecek")
         time.sleep(ekran_bekleme)
+
+
 check()
